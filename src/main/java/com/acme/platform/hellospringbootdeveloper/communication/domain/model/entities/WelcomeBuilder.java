15717a2d68210eb55db13f4ec140559725610a24
Angel Velasquez <pcisavel@upc.edu.pe>
2024-08-21 12:23:37 -0500

    fix: changed method scope to public.

--- src/main/java/com/acme/platform/hellospringbootdeveloper/communication/domain/model/entities/WelcomeBuilder.java
+++ src/main/java/com/acme/platform/hellospringbootdeveloper/communication/domain/model/entities/WelcomeBuilder.java
@@ -4,7 +4,7 @@ public class WelcomeBuilder {
     private static final String WELCOME_TEMPLATE =
             "Hello %s!. Now You are an Spring Boot Developer";
 
-    private static String build(String name) {
+    public static String build(String name) {
         String finalName = name == null ? "Developer" : name;
         return String.format(WELCOME_TEMPLATE, finalName);
     }
